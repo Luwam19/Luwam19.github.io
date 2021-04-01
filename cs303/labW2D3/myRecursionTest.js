d8a30f894093956e4416a2b138ee5fad693403a8
Luwam19 <79125146+Luwam19@users.noreply.github.com>
2021-03-31 20:12:19 -0500

    Update myRecursionTest.js

--- cs303/labW2D3/myRecursionTest.js
+++ cs303/labW2D3/myRecursionTest.js
@@ -7,7 +7,8 @@
 
   
 describe("numbers using recursion",function(){
-it("get a sum number",function (){
+
+ it("get a sum number",function (){
     assert.equal(sum(5),15)
 })
 
@@ -20,33 +21,10 @@ it("fibonacchi of a number",function (){
 })
 
 
-})
+});
 
 
 
 
 
 
-// describe("higher oder practice", function () {
-//     // beforeEach(function () {
-//     //     numArray = [1, 2, 3, 4, 5];
-//     // })
-//     it("multiply each element", function () {
-//         assert.deepEqual(myMap([1, 2, 3, 4, 5], multiplyAll), [1, 4, 9, 16, 25])
-//         assert.deepEqual(myMap([0, 2, 5, 4, 1], multiplyAll), [0, 4, 25, 16, 1])
- 
-//     })
- 
-//     it("it filters odd elements", function () {
- 
-//         assert.deepEqual(myFilter([1, 2, 3, 4, 5], oddValue), [1, 3, 5])
-//         assert.deepEqual(myFilter([11, 1, 71, 4, 5], oddValue), [11,1, 71, 5])
-//     })
- 
-//     it("it sums values", function () {
- 
-//         assert.strictEqual(myreduce([1, 2, 3, 4, 5], sumValue), 15)
-//         assert.strictEqual(myreduce([11, 9, 20, 10, 5], sumValue), 55)
-//     })
- 
-// })
