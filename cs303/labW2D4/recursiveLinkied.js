1f4e926add5a7ba7e8121d99ad4575a5ee613ec1
Luwam19 <79125146+Luwam19@users.noreply.github.com>
2021-04-02 09:11:37 -0500

    Update recursiveLinkied.js

--- cs303/labW2D4/recursiveLinkied.js
+++ cs303/labW2D4/recursiveLinkied.js
@@ -38,4 +38,79 @@ function printListReverse(list) {
     return ""
 
 }
-    //console.log(printListReverse(list))
+//console.log(printListReverse(list))
+
+
+
+class TreeNode {
+    constructor(value) {
+        this.value = value;
+        this.descendents = [];
+    }
+}
+
+const abe = new TreeNode('Abe');
+const homer = new TreeNode('Homer');
+const bart = new TreeNode('Bart');
+const lisa = new TreeNode('Lisa');
+const maggie = new TreeNode('Maggie');
+
+abe.descendents.push(homer);
+homer.descendents.push(bart, lisa, maggie);
+
+function printName(node) {
+    console.log(node.value)
+    if (node.descendents === []) {
+        return "end";
+    }
+    else {
+
+        for (let descendent of node.descendents) {
+            printName(descendent);
+        }
+    }
+}
+
+console.log(printName(abe));
+
+
+
+class TreeNode {
+    constructor(value) {
+        this.value = value;
+        this.descendents = [];
+    }
+}
+
+const abe = new TreeNode('Abe');
+const homer = new TreeNode('Homer');
+const bart = new TreeNode('Bart');
+const lisa = new TreeNode('Lisa');
+const maggie = new TreeNode('Maggie');
+
+abe.descendents.push(homer);
+homer.descendents.push(bart, lisa, maggie);
+
+function contains(node, target) {
+    if (node.descendents === null) {
+        if (node.value === target) {
+            return true;
+        } else {
+            return false;
+        }
+    } else { 
+        let childResult = false;
+        if (node.value === target) {
+            return true;
+        }
+        for (let child of node.descendents) {
+            childResult = contains(child, target);
+            if (childResult === true) {
+                return true;
+            }
+        }
+        return false; 
+    }
+}
+console.log(contains(homer, "Abe"));
+console.log(contains(homer, "Maggie")); 
